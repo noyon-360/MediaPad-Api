2e2e4732aa5aa7803339ca7f8af32172943d22a4
Nazibullah Noyon <nazibullahnoyon19.20@gmail.com>
2025-02-10 23:24:28 +0600

    MongoDB tiemr problem solve

--- Models/DB.js
+++ Models/DB.js
@@ -1,8 +1,28 @@
 const mongoose = require('mongoose');
 const mongoUri = process.env.MONGO_URI;
 
-mongoose.connect(mongoUri).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((error) => {
-    console.log(error);
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true, // Use new URL parser
+  useUnifiedTopology: true, // Use new server discovery and monitoring engine
+  serverSelectionTimeoutMS: 30000, // Increase server selection timeout to 30 seconds
+  socketTimeoutMS: 45000, // Increase socket timeout to 45 seconds
 })
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
+
+// Optional: Add event listeners for better debugging
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error('Mongoose connection error:', err);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Mongoose disconnected from MongoDB');
+});
